4ab7d0131badd5e20e4fdf7927b55c4afe0e2f2b
mjw <867331995@qq.com>
2022-07-27 15:46:40 +0800

    first commit

---
+++ src/main/java/com/mjw/git/gitTest.java
@@ -0,0 +1,12 @@
+package com.mjw.git;
+
+/**
+ * @author 草原一只鹰
+ * @create 2022-07-27 15:38
+ */
+public class gitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
