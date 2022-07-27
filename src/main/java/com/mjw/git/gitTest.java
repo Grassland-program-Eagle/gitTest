cb83a7a4a390449f128dd01d52404425d9cda79d
mjw <867331995@qq.com>
2022-07-27 16:03:26 +0800

    hot-fix commit

--- src/main/java/com/mjw/git/gitTest.java
+++ src/main/java/com/mjw/git/gitTest.java
@@ -10,5 +10,6 @@ public class gitTest {
         System.out.println("hello");
         System.out.println("hello1");
         System.out.println("hello1");
+        System.out.println("hello2");
     }
 }
